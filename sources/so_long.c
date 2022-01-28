dcf75a50535f03b8718a7e0a35306bdb7365c634
kiGzz <azn_kiguel@hotmail.fr>
2022-01-28 16:20:45 +0100

    why not working

--- sources/so_long.c
+++ sources/so_long.c
@@ -9,6 +9,11 @@
 #define GREEN_PIXEL 0xFF00
 #define WHITE_PIXEL 0xFFFFFF
 
+typedef struct s_vect
+{
+	int x;
+	int y;
+} t_vect;
 
 typedef struct s_rect
 {
@@ -26,6 +31,7 @@ typedef struct s_img
 	int		bpp; /* bits per pixel */
 	int		line_len;
 	int		endian;
+	t_vect	size;
 }	t_img;
 
 typedef struct s_data
@@ -33,8 +39,16 @@ typedef struct s_data
 	void	*mlx_ptr;
 	void	*win_ptr;
 	t_img	img;
+	t_vect	img_position;
 }	t_data;
 
+int	handle_keyrelease(int keysym, void *data)
+{
+	printf("Keyrelease: %d\n", keysym);
+	if (keysym == XK_0)
+		printf("Salope\n");
+	return (0);
+}
 
 int	handle_keypress(int keysym, t_data *data)
 {
@@ -85,11 +99,6 @@ int render_rect(t_img *img, t_rect rect)
 			img_pix_put(img, j++, i, rect.color);
 		++i;
 	}
-	j = 0;
-	while (j < WINDOW_WIDTH)
-	{
-		img_pix_put(img, j++, WINDOW_HEIGHT / 2, RED_PIXEL);
-	}
 	return (0);
 }
 
@@ -97,14 +106,24 @@ int	render(t_data *data)
 {
 	if (data->win_ptr == NULL)
 		return (1);
-	render_background(&data->img, WHITE_PIXEL);
-	render_rect(&data->img, (t_rect){WINDOW_WIDTH - 100, WINDOW_HEIGHT - 100,
-									 100, 100, GREEN_PIXEL});
+//	render_background(&data->img, WHITE_PIXEL);
+	render_rect(&data->img, (t_rect){WINDOW_WIDTH - 500, WINDOW_HEIGHT - 500,
+									 500, 500, GREEN_PIXEL});
 	render_rect(&data->img, (t_rect){0, 0, 500, 300, RED_PIXEL});
 
-	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img.mlx_img, 0, 0);
 
+	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img.mlx_img, 0, 0);
 	return (0);
+}
+
+t_img ft_new_sprite(void *mlx, char *path)
+{
+	t_img img;
+
+	img.mlx_img = mlx_xpm_file_to_image(mlx, path, &img.size.x, &img.size.y);
+	img.addr = mlx_get_data_addr(img.mlx_img, &img.bpp, &img.line_len, &img.endian);
+
+	return (img);
 }
 
 
@@ -123,18 +142,23 @@ int	main(void)
 		return (MLX_ERROR);
 	}
 
-
-	data.img.mlx_img = mlx_new_image(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT);
-	data.img.addr = mlx_get_data_addr(data.img.mlx_img, &data.img.bpp,
-									  &data.img.line_len, &data.img.endian);
+	printf("Yolo\n");
+	data.img = ft_new_sprite(data.mlx_ptr, "block.xpm");
+	data.img_position.x = 0;
+	data.img_position.y = 0;
+	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, data.img.mlx_img, data.img_position.x, data.img_position.y);
 
 	/* Setup hooks */
+//	data.img.mlx_img = mlx_new_image(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT);
+
 	mlx_loop_hook(data.mlx_ptr, &render, &data);
 	mlx_hook(data.win_ptr, KeyPress, KeyPressMask, &handle_keypress, &data);
+//	mlx_hook(data.win_ptr, KeyRelease, KeyReleaseMask, &handle_keyrelease, &data); /* CHANGED */
 
 	mlx_loop(data.mlx_ptr);
 
 	/* we will exit the loop if there's no window left, and execute this code */
+
 	mlx_destroy_image(data.mlx_ptr, data.img.mlx_img);
 	mlx_destroy_display(data.mlx_ptr);
 	free(data.mlx_ptr);
